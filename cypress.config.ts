5616874674dee92728e9d53cbd97cb06252c2047
Diego Bedolla <dbedollaruiz@gmail.com>
2024-07-10 05:18:19 -0600

    updating general config

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,7 +10,5 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'https://www.google.com',
-    setupNodeEvents(on, config) {
-    },
   },
 });
--- cypress/pages/HomePage.ts
+++ cypress/pages/HomePage.ts
@@ -3,12 +3,7 @@ class HomePage {
     private searchArea = 'textarea[title=Buscar]';
   
     visitHomePage() {
-      cy.visit('/', {
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        },
-        failOnStatusCode: false
-      });
+      cy.visit('/');
     }
 
     searchText(searchText:string){
